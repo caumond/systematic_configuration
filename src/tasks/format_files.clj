3fada0d99c901c804c4c7fcd96a3522e5e18075c
caumond <anthony@caumond.com>
2023-11-01 15:44:36 +0100

    Add file formatter

---
+++ src/tasks/format_files.clj
@@ -0,0 +1,15 @@
+(ns tasks.format-files
+  (:require [cmds]))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn format-files
+  "Format-Files
+  Params:
+  * `os` kewyord among `[:macos, :ubuntu]`"
+  [_os]
+  (cmds/execute-cmd ["fd -tf -e clj -e edn -x zprint -w {}"]))
+
+(comment
+  (format-files :macos)
+  ;
+  )

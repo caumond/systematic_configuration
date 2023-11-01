6ef1d65ac9061eca72d99533875d9e4812299558
caumond <anthony@caumond.com>
2023-11-01 15:15:01 +0100

    Add install task

---
+++ src/tasks/install.clj
@@ -0,0 +1,25 @@
+(ns tasks.install
+  (:require
+   [cmds]
+   [cfg-items]))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn install
+  "Instal
+  Params:
+  * `os` kewyord among `[:macos, :ubuntu]`"
+  [os]
+  (let [cfg-items (->> (cfg-items/read-configuration os)
+                       (filter (fn [[_ v]]
+                                 (:install v))))]
+    (println (format "Install following tasks `%s`"
+                     (mapv first cfg-items)))
+    (doseq [cfg-item cfg-items]
+      (when-let [[cfg-item {:keys [install]}] cfg-item]
+        (println (format "Install `%s`" cfg-item))
+        (cmds/execute-cmds install)))))
+
+(comment
+  (install :macos)
+  ;
+  )

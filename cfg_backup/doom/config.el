86bfbaee138e1ab0f7e9ad79f53d7ce0ee019330
caumond <anthony@caumond.com>
2024-06-08 15:32:08 +0200

    With no clojurec-mode issue, nearly comprehensive

--- cfg_backup/doom/config.el
+++ cfg_backup/doom/config.el
@@ -1,10 +1,9 @@
-;; ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; ;; Place your private configuration here! Remember, you do not need to run 'doom
-;; ;; sync' after modifying this file!
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-;; ;; Note:
-;; ;; How to set a variable (with or without after!) is described here: https://github.com/doomemacs/doomemacs/blob/master/docs/faq.org#changes-to-my-config-arent-taking-effect
+
 
 ;; ;; To update version control regularly, even if the file is not modified.
 ;; (setq auto-revert-check-vc-info t)
@@ -65,118 +64,114 @@
 ;; ;;   (add-to-list 'tree-sitter-major-mode-language-alist '(clojurec-mode . clojure))
 ;; ;;   (add-to-list 'tree-sitter-major-mode-language-alist '(clojurescript-mode . clojure)))
 
-;; (setq auth-sources '("~/.authinfo.gpg"))
 
-;; ;; Mermaid-mode
-;; ;; Automatically activate mermaid-mode as a major mode when opening `.mermaid' suffix files.
-;; (add-to-list 'auto-mode-alist '("\\.mermaid\\'" . mermaid-mode))
+(setq auth-sources '("~/.authinfo.gpg"))
+
+;; Mermaid-mode
+;; Automatically activate mermaid-mode as a major mode when opening `.mermaid' suffix files.
+(add-to-list 'auto-mode-alist '("\\.mermaid\\'" . mermaid-mode))
 
 
-;; ;; lsp grammarly
-;; (use-package! lsp-grammarly
-;;   :ensure t
-;;   :hook (text-mode . (lambda ()
-;;                        (require 'lsp-grammarly)
-;;                        (lsp)
-;;                        (set-lsp-priority! 'lsp-grammarly 2))))
+;; lsp grammarly
+(use-package! lsp-grammarly
+  :ensure t
+  :hook (text-mode . (lambda ()
+                       (require 'lsp-grammarly)
+                       (lsp)
+                       (set-lsp-priority! 'lsp-grammarly 2))))
 
-;; ;; lsp clojure
-;; (after! lsp-clojure
-;;   (setq lsp-lens-enable nil))
+;; lsp clojure
+(after! lsp-clojure
+  (setq lsp-lens-enable nil))
 
-;; ;; hephaistox
-;; ;;(map! "C-M-x" #'call-hephaistox)
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Anthony CAUMOND"
+       user-mail-address "anthony@caumond.com")
 
-;; ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; ;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "Anthony CAUMOND"
-;;       user-mail-address "anthony@caumond.com")
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-;; ;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;; ;;
-;; ;; - `doom-font' -- the primary font to use
-;; ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;; ;;   presentations or streaming.
-;; ;; - `doom-unicode-font' -- for unicode glyphs
-;; ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;; ;;
-;; ;; See 'C-h v doom-font' for documentation and more examples of what they
-;; ;; accept. For example:
-;; ;;
-;; ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;; ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;; ;;
-;; ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; ;; refresh your font settings. If Emacs still can't find your font, it likely
-;; ;; wasn't installed correctly. Font issues are rarely Doom issues!
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
 
-;; ;; There are two ways to load a theme. Both assume the theme is installed and
-;; ;; available. You can either set `doom-theme' or manually load a theme with the
-;; ;; `load-theme' function. This is the default:
-;; ;; See https://github.com/doomemacs/themes#theme-list
-;; ;;'doom-one
-;; (setq doom-theme 'doom-dracula)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
-;; ;; This determines the style of line numbers in effect. If set to `nil', line
-;; ;; numbers are disabled. For relative line numbers, set this to `relative'.
-;; (setq display-line-numbers t)
-
-;; ;; If you use `org' and don't want your org files in the default location below,
-;; ;; change `org-directory'. It must be set before org loads!
-;; ;; Thesse variables are a special case of variables that should be set outside after!
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+;; Org non active anymore so commented out
 ;; (setq org-directory "~/Dev/perso/my-notes/todos")
 ;; (setq! org-agenda-files
 ;;        '("~/Dev/perso/my-notes/todos/main.org"))
-;; (after! org
-;;   (setq org-duration-format (quote h:mm)))
+(setq org-directory "~/org/")
 
-;; ;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;; ;;
-;; ;;   (after! PACKAGE
-;; ;;     (setq x y))
-;; ;;
-;; ;; The exceptions to this rule:
-;; ;;
-;; ;;   - Setting file/directory variables (like `org-directory')
-;; ;;   - Setting variables which explicitly tell you to set them before their
-;; ;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;; ;;   - Setting doom variables (which start with 'doom-' or '+').
-;; ;;
-;; ;; Here are some additional functions/macros that will help you configure Doom.
-;; ;;
-;; ;; - `load!' for loading external *.el files relative to this one
-;; ;; - `use-package!' for configuring packages
-;; ;; - `after!' for running code after a package has loaded
-;; ;; - `add-load-path!' for adding directories to the `load-path', relative to
-;; ;;   this file. Emacs searches the `load-path' when you load packages with
-;; ;;   `require' or `use-package'.
-;; ;; - `map!' for binding new keys
-;; ;;
-;; ;; To get information about any of these functions/macros, move the cursor over
-;; ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; ;; This will open documentation for it, including demos of how they are used.
-;; ;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; ;; etc).
-;; ;;
-;; ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; ;; they are implemented.
+(after! org
+   (setq org-duration-format (quote h:mm)))
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(setq! initial-frame-alist
+        '((fullscreen . maximized)))
 
-;; (setq! initial-frame-alist
-;;        '((fullscreen . maximized)))
-
-;; ;; C Control
-;; ;; M Option
-;; ;; s Command
-;; ;; S Shift
-;; (map! "C-M-;" #'babashka-project-tasks)
-;; (map! "M-s-t" #'projectile-toggle-between-implementation-and-test)
-;; (map! "C-M-v" #'sp-forward-slurp-sexp)
-;; (map! "C-k" #'sp-kill-sexp)
-;; (map! "C-M-k" #'sp-kill-word)
-;; (map! "C-M-s-r" #'sp-raise-sexp)
-;; (map! "C-M-è" #'sp-wrap-curly)
-;; (map! "C-M-(" #'sp-wrap-round)
-;; (map! "C-M-§" #'sp-wrap-square)
+;; C Control
+;; M Option
+;; s Command
+;; S Shift
+(map! "C-M-;" #'babashka-project-tasks)
+(map! "M-s-t" #'projectile-toggle-between-implementation-and-test)
+(map! "C-M-v" #'sp-forward-slurp-sexp)
+(map! "C-k" #'sp-kill-sexp)
+(map! "C-M-k" #'sp-kill-word)
+(map! "C-M-s-r" #'sp-raise-sexp)
+(map! "C-M-è" #'sp-wrap-curly)
+(map! "C-M-(" #'sp-wrap-round)
+(map! "C-M-§" #'sp-wrap-square)
--- cfg_backup/doom/init.el
+++ cfg_backup/doom/init.el
@@ -21,42 +21,43 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       company             ; the ultimate code completion backend
+       ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;ivy                 ; a search engine for love and life
-       vertico           ; the search engine of the future
+       ;;ivy               ; a search engine for love and life
+       vertico             ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       doom                ; what makes DOOM look the way it does
+       doom-dashboard      ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;hydra
+       (emoji +unicode)    ; 🙂
+       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures           ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       modeline          ; snazzy, Atom-inspired modeline, plus API
+       modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
-       ;;ophints           ; highlight the region an operation acts on
+       ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       treemacs ;(treemacs ); +lsp     ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       treemacs            ; a project drawer, like neotree but cooler
+       unicode             ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
-       ;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
+       vi-tilde-fringe     ; fringe tildes to mark beyond EOB
+       ;;window-select     ; visually switch windows
+       workspaces          ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
-       (format)  ; automated prettiness
+       (evil +everywhere)  ; come to the dark side, we have cookies
+       file-templates      ; auto-snippets for empty files
+       fold                ; (nigh) universal code folding
+       (format)            ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -87,36 +88,36 @@
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       lsp                 ; M-x vscode
-       (magit +forge)      ; a git porcelain for Emacs
+       lsp               ; M-x vscode
+       magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements gg
+       ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       ;; tmux              ; an API for interacting with tmux
-      ;; tree-sitter       ; syntax and parsing, sitting in a tree...
+       ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;(cc +lsp)         ; C > C++ == 1
-       (clojure +lsp ) ;+tree-sitter           ; java with a lisp
+       (clojure +lsp)      ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
